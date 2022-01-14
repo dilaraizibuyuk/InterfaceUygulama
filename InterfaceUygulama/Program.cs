79ca609ee8301edf3ecc1f21a94ab7abacb5ebfc
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-14 16:25:09 +0300

    Proje dosyası ekle.

---
+++ InterfaceUygulama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUygulama
+{
+    class Program
+    {
+        public interface Islem
+        {
+            void topla(int sayi1, int sayi2);
+            void carp(int sayi1, int sayi2);
+        }
+        public class Hesap : Islem
+        {
+            public void carp(int sayi1,int sayi2)
+            {
+                Console.WriteLine(sayi1+"x"+sayi2+"="+sayi2*sayi1);
+            }
+            public void topla(int sayi1, int sayi2)
+            {
+                Console.WriteLine(sayi1 + "+" + sayi2 + "=" + (sayi2 + sayi1));
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Hesap hesap = new Hesap();
+            hesap.topla(5,10);
+            hesap.carp(5,10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InterfaceUygulama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("InterfaceUygulama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterfaceUygulama")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("23f505e8-a4f9-4dd2-9b0f-1f077a36c5b1")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
